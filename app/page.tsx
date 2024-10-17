119754d54d6ea7a058a72ba2540ecec1aff25ea7
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-10-17 16:05:21 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <>
     <main>
     <head>
-        <meta name="google-site-verification" content="M4Pw1J2X-V6c2FotMav2KX72M48-Zvp0Lhnc9Q4qOnI" />
+       <meta name="google-site-verification" content="YYTGdng2CumMVhVlK1mX4XHvwnSaTHncdYAVWsi75GQ" />
     </head>
       <Hero />
       <Featured />
