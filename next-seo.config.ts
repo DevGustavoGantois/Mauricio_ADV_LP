e3bc704169be10ac06bf6d2b3cadcb44385c28c4
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-10-17 16:10:27 -0300

    Update next-seo.config.ts

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -6,13 +6,13 @@ export const metadata: Metadata = {
       'O Escritório de Advocacia Maurício Sampaio oferece serviços jurídicos de alta qualidade, com uma equipe de advogados experientes e especializados em diversas áreas do Direito, incluindo Direito Civil, Direito Empresarial, Direito Tributário e Direito Trabalhista. Nossa missão é proporcionar soluções jurídicas eficazes e personalizadas, garantindo a proteção dos direitos e interesses de nossos clientes. Nosso escritório se destaca pelo compromisso com a ética, transparência e atendimento humanizado. Trabalhamos tanto com consultoria preventiva quanto com a defesa em processos judiciais, sempre buscando a melhor estratégia para cada caso. Se você precisa de assessoria jurídica confiável, entre em contato conosco. No Escritório de Advocacia Maurício Sampaio, seu problema é nossa prioridade!',
     openGraph: {
       type: 'website',
-      url: 'http://msampaio.adv.br/',
+      url: 'https://msampaio-adv.vercel.app/',
       title: 'Escritório de Advocacia Maurício Sampaio',
       description:
         'O Escritório de Advocacia Maurício Sampaio oferece serviços jurídicos de alta qualidade, com uma equipe de advogados experientes e especializados em diversas áreas do Direito, incluindo Direito Civil, Direito Empresarial, Direito Tributário e Direito Trabalhista. Nossa missão é proporcionar soluções jurídicas eficazes e personalizadas, garantindo a proteção dos direitos e interesses de nossos clientes.',
       images: [
         {
-          url: 'http://msampaio.adv.br/IMGLOGO.png',
+          url: 'https://msampaio-adv.vercel.app/IMGLOGO.png',
           width: 800,
           height: 800,
           alt: 'Logo Escritório',
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
       title: 'Escritório de Advocacia Maurício Sampaio',
       description:
         'O Escritório de Advocacia Maurício Sampaio oferece serviços jurídicos de alta qualidade, com uma equipe de advogados experientes e especializados em diversas áreas do Direito, incluindo Direito Civil, Direito Empresarial, Direito Tributário e Direito Trabalhista.',
-      images: ['http://msampaio.adv.br/IMGLOGO.png'],
+      images: ['https://msampaio-adv.vercel.app/IMGLOGO.png'],
     },
     robots: {
       index: true,
