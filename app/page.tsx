ff0c6661a7e4d767f78bf94bab1e3ce133f6dd0c
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-10-17 15:42:45 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,49 +7,14 @@ import { Hero } from "@/components/Hero";
 import { LocationSection } from "@/components/LocationSection";
 import { Testimonials } from "@/components/Testimonials";
 
-//import meta tags of site.
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-    title: 'Escritório de Advocacia Maurício Sampaio',
-    description:
-      'O Escritório de Advocacia Maurício Sampaio oferece serviços jurídicos de alta qualidade, com uma equipe de advogados experientes e especializados em diversas áreas do Direito, incluindo Direito Civil, Direito Empresarial, Direito Tributário e Direito Trabalhista. Nossa missão é proporcionar soluções jurídicas eficazes e personalizadas, garantindo a proteção dos direitos e interesses de nossos clientes. Nosso escritório se destaca pelo compromisso com a ética, transparência e atendimento humanizado. Trabalhamos tanto com consultoria preventiva quanto com a defesa em processos judiciais, sempre buscando a melhor estratégia para cada caso. Se você precisa de assessoria jurídica confiável, entre em contato conosco. No Escritório de Advocacia Maurício Sampaio, seu problema é nossa prioridade!',
-    openGraph: {
-      type: 'website',
-      url: 'https://www.Meuovo1234.com.br',
-      title: 'Escritório de Advocacia Maurício Sampaio',
-      description:
-        'O Escritório de Advocacia Maurício Sampaio oferece serviços jurídicos de alta qualidade, com uma equipe de advogados experientes e especializados em diversas áreas do Direito, incluindo Direito Civil, Direito Empresarial, Direito Tributário e Direito Trabalhista. Nossa missão é proporcionar soluções jurídicas eficazes e personalizadas, garantindo a proteção dos direitos e interesses de nossos clientes.',
-      images: [
-        {
-          url: 'https://www.MeuOvo1234.com.br/IMGLOGO.png',
-          width: 800,
-          height: 800,
-          alt: 'Logo Escritório',
-        },
-      ],
-      siteName: 'Escritório Maurício Sampaio',
-    },
-    twitter: {
-      card: 'summary_large_image',
-      site: '@EscritórioMaurícioSampaio',
-      creator: '@mauricioSampaio',
-      title: 'Escritório de Advocacia Maurício Sampaio',
-      description:
-        'O Escritório de Advocacia Maurício Sampaio oferece serviços jurídicos de alta qualidade, com uma equipe de advogados experientes e especializados em diversas áreas do Direito, incluindo Direito Civil, Direito Empresarial, Direito Tributário e Direito Trabalhista.',
-      images: ['https://www.MeuOvo1234.com.br/IMGLOGO.png'],
-    },
-    robots: {
-      index: true,
-      follow: true,
-    },
-  };
-
 export default function Home() {
 
   return (
     <>
     <main>
+    <head>
+        <meta name="google-site-verification" content="M4Pw1J2X-V6c2FotMav2KX72M48-Zvp0Lhnc9Q4qOnI" />
+    </head>
       <Hero />
       <Featured />
       <AboutOffice />
