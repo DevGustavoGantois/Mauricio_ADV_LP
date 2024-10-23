f4dd0d726403b93283b2a802e34c217cb185b0a8
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-10-22 22:42:13 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <>
     <main>
     <Head>
-    <meta name="google-site-verification" content="M4Pw1J2X-V6c2FotMav2KX72M48-Zvp0Lhnc9Q4qOnI" />
+    <meta name="google-site-verification=YYTGdng2CumMVhVlK1mX4XHvwnSaTHncdYAVWsi75GQ" />
     <title>Escritório de Advocacia Maurício Sampaio</title>
     </Head>
       <Hero />
